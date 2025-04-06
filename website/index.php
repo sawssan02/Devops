802a615054ef3448a3f58b49b96cda64c163cfeb
sawssan <sawssanatiq@gmail.com>
2025-04-06 00:49:07 +0000

    Update index.php

--- website/index.php
+++ website/index.php
@@ -12,11 +12,9 @@
 
             <?php
               if ($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['submit'])) {
-                  // Hardcoded credentials for the API authentication
+                  
                   $username = 'root';
                   $password = 'root';
-
-                  // Prepare the context for HTTP request with basic authentication header
                   $context = stream_context_create(array(
                     "http" => array(
                       "header" => "Authorization: Basic " . base64_encode("$username:$password")
@@ -25,17 +23,13 @@
 
                   $url = 'http://13.61.3.10:5000/supmit/api/v1.0/get_student_ages';
 
-                  // Attempt to fetch the data from the API
                   $response = @file_get_contents($url, false, $context);
 
-                  // Check if the request was successful
                   if ($response === FALSE) {
                       echo "<p style='color:red;'>Error: Unable to fetch data from the API.</p>";
                   } else {
-                      // Decode the JSON response from the API
                       $list = json_decode($response, true);
 
-                      // Check if the data contains the student ages
                       if (isset($list["student_ages"]) && is_array($list["student_ages"])) {
                           echo "<p style='color:red; font-size: 20px;'>This is the list of the student with age:</p>";
                           foreach ($list["student_ages"] as $key => $value) {
